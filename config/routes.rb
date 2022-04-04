a61be64750cbafb36e5dad0a1615e1060bc40036
ayano309 <ayatomato.k@gmail.com>
2022-04-04 06:41:27 +0000

    [Finish]5-１4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
+  
   devise_for :users
   root to: "homes#top"
   get  'homes/about', to: 'homes#about', as: 'about'
   
-  resources :post_images, only: [:new, :create, :index, :show, :destroy
-  ]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
 end
